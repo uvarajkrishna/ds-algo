05866382beb9fc9398a8eae7ff034b83b257cbfd
Uvaraj Krishnaswamy <uvaraj.krishnaswamy@nium.com>
2022-10-30 15:34:44 +0530

    Documentation Updates

--- docs/Math.md
+++ docs/Math.md
@@ -48,15 +48,52 @@ sliced using modulo of ten.
       return true;
     }
     int reversedNumber = 0;
-    int originalNumber = number;
-    while (number > 0) {
-      reversedNumber = reversedNumber * 10 + number % 10;
-      number = number / 10;
+    int originalNumber=number;
+    while(number>0){
+    reversedNumber=reversedNumber*10+number%10;
+    number=number/10;
     }
-    return originalNumber == reversedNumber;
-  }
+    return originalNumber==reversedNumber;
+    }
 ```
+
 ## Factorial of a Number
+
+**Problem -** Find factorial of the given number.
+
+| Input |                Process | Outcome |
+|------:|-----------------------:|--------:|
+|     4 |          1 x 2 x 3 x 4 |      24 |
+|     6 |  1 x 2 x 3 x 4 x 5 x 6 |     720 |
+|    10 | 1x 2 x 3. . . x 9 x 10 | 3628800 |
+
+**Iterative Approach**
+
+Time Complexity - θ(N), Auxilary Space Complexity - θ(1)
+
+```java
+  private static int factorialThroughIteration(final int number){
+    int factorial=1;
+    for(int i=1;i<=number;i++){
+    factorial=factorial*i;
+    }
+    return factorial;
+    }
+
+```
+
+**Recursive Approach**
+
+Time Complexity - θ(N), Auxilary Space Complexity - θ(1)
+
+```java
+  private static int factorialThroughRecursion(int number){
+    if(number==0){
+    return 1;
+    }
+    return number*factorialThroughRecursion(number-1);
+    }
+```
 
 ## Trailing Zeros in Factorial
 
--- src/main/java/com/math/Factorial.java
+++ src/main/java/com/math/Factorial.java
@@ -1,20 +1,23 @@
 package com.math;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SuppressWarnings({"java:S5960", "java:S106"})
 public class Factorial {
 
   public static void main(String[] args) {
-    assert factorialIterative(4) == 24;
-    assert factorialIterative(6) == 720;
-    assert factorialIterative(0) == 0;
+    assertThat(factorialThroughIteration(4)).isEqualTo(24);
+    assertThat(factorialThroughIteration(6)).isEqualTo(720);
+    assertThat(factorialThroughIteration(0)).isEqualTo(1);
 
-    assert factorialRecursive(4) == 24;
-    assert factorialRecursive(6) == 720;
-    assert factorialRecursive(0) == 1;
-    assert factorialRecursive(1) == 1;
+    assertThat(factorialThroughRecursion(4)).isEqualTo(24);
+    assertThat(factorialThroughRecursion(6)).isEqualTo(720);
+    assertThat(factorialThroughRecursion(0)).isEqualTo(1);
+    assertThat(factorialThroughRecursion(1)).isEqualTo(1);
+    assertThat(factorialThroughRecursion(10)).isEqualTo(3628800);
   }
 
-  private static int factorialIterative(final int number) {
+  private static int factorialThroughIteration(final int number) {
     int factorial = 1;
     for (int i = 1; i <= number; i++) {
       factorial = factorial * i;
@@ -22,10 +25,10 @@ public class Factorial {
     return factorial;
   }
 
-  private static int factorialRecursive(int number) {
+  private static int factorialThroughRecursion(int number) {
     if (number == 0) {
       return 1;
     }
-    return number * factorialRecursive(number - 1);
+    return number * factorialThroughRecursion(number - 1);
   }
 }
